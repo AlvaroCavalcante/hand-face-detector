d3d2a293938e1bedcb9ab30975efb1cd65b208be
Alvarole <leandro0807@live.com>
2021-01-10 15:56:56 -0300

    predicting hands from my dataset

--- hand_detection2.py
+++ hand_detection2.py
@@ -17,7 +17,7 @@ from object_detection.builders import model_builder
 PATH_TO_CFG = "./pipeline.config"
 PATH_TO_CKPT = "./checkpoint"
 PATH_TO_LABELS = './label_map.pbtxt'
-IMAGE_PATHS = glob.glob('./hand_images/*.jpg')
+IMAGE_PATHS = '/home/alvaro/Documentos/projeto libras/frame dataset/20 FPS hand/train/'
 
 print('Loading model... ', end='')
 
@@ -46,46 +46,51 @@ def load_image_into_numpy_array(path):
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
                                                                     use_display_name=True)
 
-for image_path in IMAGE_PATHS:
-    print('Running inference for {}... '.format(image_path), end='')
+for word in os.listdir(IMAGE_PATHS):
+    for image_name in os.listdir(IMAGE_PATHS+word):
+        print('Running inference for {}... '.format(image_name), end='')
 
-    image_np = load_image_into_numpy_array(image_path)
-    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+        image_path = IMAGE_PATHS+word+'/'+image_name
+        image_np = load_image_into_numpy_array(image_path)
+        input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
 
-    detections = detect_fn(input_tensor)
+        detections = detect_fn(input_tensor)
 
-    # All outputs are batches tensors.
-    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-    # We're only interested in the first num_detections.
-    num_detections = int(detections.pop('num_detections'))
-    detections = {key: value[0, :num_detections].numpy()
-                  for key, value in detections.items()}
-    detections['num_detections'] = num_detections
+        # All outputs are batches tensors.
+        # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+        # We're only interested in the first num_detections.
+        num_detections = int(detections.pop('num_detections'))
+        detections = {key: value[0, :num_detections].numpy()
+                    for key, value in detections.items()}
+        detections['num_detections'] = num_detections
 
-    # detection_classes should be ints.
-    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+        # detection_classes should be ints.
+        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
 
-    label_id_offset = 1
-    image_np_with_detections = image_np.copy()
+        label_id_offset = 1
+        image_np_with_detections = image_np.copy()
 
-    bouding_boxes = result = viz_utils.visualize_boxes_and_labels_on_image_array(
-            image_np_with_detections,
-            detections['detection_boxes'],
-            detections['detection_classes']+label_id_offset,
-            detections['detection_scores'],
-            category_index,
-            use_normalized_coordinates=True,
-            max_boxes_to_draw=200,
-            min_score_thresh=.30,
-            agnostic_mode=False)
+        bouding_boxes = result = viz_utils.visualize_boxes_and_labels_on_image_array(
+                image_np_with_detections,
+                detections['detection_boxes'],
+                detections['detection_classes']+label_id_offset,
+                detections['detection_scores'],
+                category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=200,
+                min_score_thresh=.30,
+                agnostic_mode=False)
 
-    im_width, im_height = image_np.shape[1], image_np.shape[0]
+        im_width, im_height = image_np.shape[1], image_np.shape[0]
+        image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)  
+        count = 0
 
-    count = 0
-    for box in bouding_boxes:
-        xmin, xmax, ymin, ymax = box['xmin'], box['xmax'], box['ymin'], box['ymax']
-        xmin, xmax, ymin, ymax = int(xmin * im_width), int(xmax * im_width), int(ymin * im_height), int(ymax * im_height)
+        for box in bouding_boxes:
+            xmin, xmax, ymin, ymax = box['xmin'], box['xmax'], box['ymin'], box['ymax']
+            xmin, xmax, ymin, ymax = int(xmin * im_width), int(xmax * im_width), int(ymin * im_height), int(ymax * im_height)
 
-        cv2.imwrite('just_hand_' +str(count)+'.jpg', image_np[ymin:ymax, xmin:xmax, :])
-        cv2.imwrite('hand_infer.jpg', image_np_with_detections)
-        count +=1
+            save_path = IMAGE_PATHS+word+'/'+image_name.split('.jpg')[0]+'hand_'+str(count) +'.jpg'
+
+            cv2.imwrite(save_path, image_np[ymin:ymax, xmin:xmax, :])
+            cv2.imwrite('hand_infer.jpg', image_np_with_detections)
+            count +=1
