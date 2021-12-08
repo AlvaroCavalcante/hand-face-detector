e4fc403865fa6d0f2a65ee777ae3a1596aa06484
Alvaro Leandro <leandro0807@live.com>
2021-12-08 20:39:35 -0300

    adding arguments in the infer algorithm

--- hand_face_detection.py
+++ hand_face_detection.py
@@ -1,20 +1,19 @@
+from os import name
 import tensorflow as tf
 import numpy as np
 import cv2
 import time
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as viz_utils
+import argparse
 
 physical_devices = tf.config.list_physical_devices('GPU')
 try:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 except:
-    # Invalid device or cannot modify virtual devices once initialized.
-    pass
+    print('GPU not found')
 
-# tf.config.optimizer.set_jit(True)
-
-def infer_images(image, label_map_path, draw_image=False):
+def infer_images(image, label_map_path, detect_fn):
     image_np = np.array(image)
     input_tensor = tf.convert_to_tensor(image_np)
     input_tensor = input_tensor[tf.newaxis, ...]
@@ -33,53 +32,63 @@ def infer_images(image, label_map_path, draw_image=False):
     
     image_np_with_detections = image_np.copy()
 
-    if draw_image:
-      viz_utils.visualize_boxes_and_labels_on_image_array(
-          image_np_with_detections,
-          detections['detection_boxes'],
-          detections['detection_classes'],
-          detections['detection_scores'],
-          category_index,
-          use_normalized_coordinates=True,
-          max_boxes_to_draw=200,
-          min_score_thresh=.30,
-          agnostic_mode=False)
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+        image_np_with_detections,
+        detections['detection_boxes'],
+        detections['detection_classes'],
+        detections['detection_scores'],
+        category_index,
+        use_normalized_coordinates=True,
+        max_boxes_to_draw=200,
+        min_score_thresh=.30,
+        agnostic_mode=False)
 
     return detections['detection_boxes'], detections['detection_classes'], detections['detection_scores'], image_np_with_detections
 
-detect_fn = tf.saved_model.load('/home/alvaro/Downloads/exported_model_2/saved_model/')
 
-cap = cv2.VideoCapture('/home/alvaro/Documentos/body-detection/utils/test_videos/asl_bench.mp4')
+def main(args):
+    detect_fn = tf.saved_model.load(args.saved_model_path)
 
-count = 0
-fps = str(0)
-start_time = time.time()
-fps_hist = []
+    source = args.source_path if args.source_path else 0
+    cap = cv2.VideoCapture(source)
 
-while True:
-    got_frame, frame = cap.read()
+    count = 0
+    fps = str(0)
+    start_time = time.time()
+    fps_hist = []
 
-    if not got_frame:
-        break
+    while True:
+        got_frame, frame = cap.read()
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # output = frame.copy()
-    frame = cv2.resize(frame, (512, 512)).astype('uint8')
-    _, _, _, img_np = infer_images(frame, '/home/alvaro/Documentos/body-detection/utils/label_map.pbtxt', True)
-    frame = np.divide(frame, 255)
+        if not got_frame:
+            break
 
-    count += 1
-    if (time.time() - start_time) > 1:
-        fps = int(count / (time.time() - start_time))
-        print("detection FPS: {}".format(str(fps)))
-        count = 0
-        start_time = time.time()
-        fps_hist.append(fps)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, (512, 512)).astype('uint8')
+        _, _, _, img_np = infer_images(frame, args.label_map_path, detect_fn)
+        frame = np.divide(frame, 255)
 
-    cv2.imshow("Frame", cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB))
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
+        count += 1
+        if (time.time() - start_time) > 1:
+            fps = int(count / (time.time() - start_time))
+            print('detection FPS: {}'.format(str(fps)))
+            count = 0
+            start_time = time.time()
+            fps_hist.append(fps)
 
-print(fps_hist)
-print('Mean FPS: ', str(sum(fps_hist) / len(fps_hist)))
+        cv2.imshow('Frame', cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB))
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+
+    print(fps_hist)
+    print('Mean FPS: ', str(sum(fps_hist) / len(fps_hist)))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--saved_model_path', type=str)
+    parser.add_argument('--source_path', type=str)
+    parser.add_argument('--label_map_path', type=str, default='./utils/label_map.pbtxt')
+    args = parser.parse_args()
+
+    main(args)
