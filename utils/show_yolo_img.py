4997df2bc30e965150c075968578b6a745512797
Alvaro Carneiro <leandro0807@live.com>
2022-05-24 22:31:45 -0300

    scripts to convert xml annotations to yolo txt

---
+++ utils/show_yolo_img.py
@@ -0,0 +1,35 @@
+from PIL import Image, ImageDraw
+
+
+def yolo_to_xml_bbox(bbox, w, h):
+    # x_center, y_center width heigth
+    w_half_len = (bbox[2] * w) / 2
+    h_half_len = (bbox[3] * h) / 2
+    xmin = int((bbox[0] * w) - w_half_len)
+    ymin = int((bbox[1] * h) - h_half_len)
+    xmax = int((bbox[0] * w) + w_half_len)
+    ymax = int((bbox[1] * h) + h_half_len)
+    return [xmin, ymin, xmax, ymax]
+
+
+def draw_image(img, bboxes):
+    draw = ImageDraw.Draw(img)
+    for bbox in bboxes:
+        draw.rectangle(bbox, outline="red", width=2)
+    img.save("example.jpg")
+    img.show()
+
+
+image_filename = "/home/alvaro/Downloads/test/signer0_sample4_color_33.jpg"
+label_filename = "/home/alvaro/Downloads/labels/signer0_sample4_color_33.txt"
+bboxes = []
+
+img = Image.open(image_filename)
+
+with open(label_filename, 'r', encoding='utf8') as f:
+    for line in f:
+        data = line.strip().split(' ')
+        bbox = [float(x) for x in data[1:]]
+        bboxes.append(yolo_to_xml_bbox(bbox, img.width, img.height))
+
+draw_image(img, bboxes)
