7374cd8301decf659717858000e3756f98fd6907
alvaro leandro <leandro0807@live.com>
2022-07-27 21:40:25 -0300

    showing bouding boxes aspect ratios

---
+++ utils/visualizations/show_boxes_aspects.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+
+
+black_img = np.zeros((350, 760, 3), dtype=np.uint8)
+
+def get_aspect_ratio_box(img, aspect_ratio, pos, color):
+    xmin, xmax = pos[0], pos[1]
+
+    ymin = 100
+    ymax = 100 + int((100 * aspect_ratio))
+
+    cv2.rectangle(img, (xmin, ymin), (xmax, ymax), color, 2)
+    return img
+
+blue = (0, 0, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
+
+black_img = get_aspect_ratio_box(black_img, 0.5, (20, 120), red)
+black_img = get_aspect_ratio_box(black_img, 0.67, (140, 240), green)
+
+black_img = get_aspect_ratio_box(black_img, 1, (260, 360), red)
+black_img = get_aspect_ratio_box(black_img, 0.91, (380, 480), green)
+
+
+black_img = get_aspect_ratio_box(black_img, 2, (500, 600), red)
+black_img = get_aspect_ratio_box(black_img, 1.26, (620, 720), green)
+
+cv2.imwrite('box.jpg', cv2.cvtColor(black_img, cv2.COLOR_BGR2RGB))
